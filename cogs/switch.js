109a8f74c73155d6e8b879c9b6ff78e1387760b0
SollyBunny <sollybunn@gmail.com>
2024-12-20 13:01:12 +0000

    Add switch command

---
+++ cogs/switch.js
@@ -0,0 +1,10 @@
+import { CommandArgNumber } from "../command";
+
+addCommand("switch", "Turn a switch on or off")
+	.addArg(new CommandArgNumber("id", "Id to switch (-1 for all)", false, -1, 999, 1))
+	.addArg(new CommandArgBool("state", "State for switch (empty for toggle)", true))
+	.addImpl(async (player, room, id, enabled) => {
+		if (!player.canEdit)
+			return;
+		room.world.globalSwitch(id, enabled);
+	});
