58875a5ef01a10ccee615ac6ea945c06fc17844d
SollyBunny <sollybunn@gmail.com>
2024-12-25 00:46:38 +0000

    Add free perms

--- cogs/freeperm.js
+++ cogs/freeperm.js
@@ -1,7 +1,32 @@
+export const name = "ping";
 
-addCommand("freeedit", "Turn freeedit on or off")
-	.addArg(new CommandArgBool("enabled", "Whether freeedit is enabled", true))
-	.addImpl(async (player, room, enabled) => {
-		room.freeEdit = enabled === undefined ? !room.freeEdit : enabled;
-		room.chat.send(`Free Edit ${room.freeEdit ? "Enabled" : "Disabled"}`);
+export async function init(client, room, commands) {
+
+	room.players.on("join", ({ player }) => {
+		if (room.freeEdit)
+			room.players.setCanEdit(true);
+		if (room.freeGod)
+			room.players.setCanGod(true);
 	});
+
+	commands.add("freeedit", "Give everyone edit")
+		.addArg(new CommandArgBool("enabled", "Whether freeedit is enabled", true))
+		.addImpl(async (player, room, enabled) => {
+			if (!player.owner) return;
+			room.freeEdit = enabled === undefined ? !room.freeEdit : enabled;
+			room.chat.send(`Free Edit ${room.freeEdit ? "Enabled" : "Disabled"}`);
+			if (room.freeEdit)
+				room.chat.send("/giveedit @a")
+		});
+
+	commands.add("freegod", "Give everyone god")
+		.addArg(new CommandArgBool("enabled", "Whether freegod is enabled", true))
+		.addImpl(async (player, room, enabled) => {
+			if (!player.owner) return;
+			room.freeGod = enabled === undefined ? !room.freeGod : enabled;
+			room.chat.send(`Free God ${room.freeEdit ? "Enabled" : "Disabled"}`);
+			if (room.freeGod)
+				room.chat.send("/givegod @a");
+		});
+
+}
